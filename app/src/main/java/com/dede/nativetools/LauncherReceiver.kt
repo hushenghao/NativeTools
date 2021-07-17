8270a5ea1416aac52987fa393492997cc00b291d
胡晟昊 <863116814@qq.com>
2021-07-17 19:46:49 +0800

    fix 覆盖安装广播无法启动问题

--- app/src/main/java/com/dede/nativetools/LauncherReceiver.kt
+++ app/src/main/java/com/dede/nativetools/LauncherReceiver.kt
@@ -6,18 +6,13 @@ import android.content.Intent
 import android.util.Log
 import androidx.core.content.ContextCompat
 import com.dede.nativetools.netspeed.NetSpeedConfiguration
-import com.dede.nativetools.netspeed.NetSpeedService
 import com.dede.nativetools.netspeed.NetSpeedConfiguration.Companion.defaultSharedPreferences
+import com.dede.nativetools.netspeed.NetSpeedService
 
 class LauncherReceiver : BroadcastReceiver() {
 
     companion object {
         const val ACTION_LAUNCHER = "com.dede.netavetools.LAUNCHER"
-        private val actions = arrayOf(
-            Intent.ACTION_BOOT_COMPLETED,
-            Intent.ACTION_MY_PACKAGE_REPLACED,
-            ACTION_LAUNCHER
-        )
 
         fun launcher(context: Context) {
             val status = defaultSharedPreferences.getBoolean(
@@ -34,21 +29,21 @@ class LauncherReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         val action = intent.action
         Log.i("LauncherReceiver", "onReceive: $action")
-        if (!actions.contains(action)) return
 
-        if (action == Intent.ACTION_BOOT_COMPLETED ||
-            action == Intent.ACTION_MY_PACKAGE_REPLACED
-        ) {
-            // 开机自启
-            val autoBoot = defaultSharedPreferences.getBoolean(
-                NetSpeedConfiguration.KEY_NET_SPEED_AUTO_START,
-                false
-            )
-            if (!autoBoot) {
-                return
+        when (action) {
+            Intent.ACTION_BOOT_COMPLETED -> {
+                // 开机自启的设置状态
+                val autoBoot = defaultSharedPreferences.getBoolean(
+                    NetSpeedConfiguration.KEY_NET_SPEED_AUTO_START,
+                    false
+                )
+                if (!autoBoot) {
+                    return
+                }
+            }
+            Intent.ACTION_MY_PACKAGE_REPLACED, ACTION_LAUNCHER -> {
+                launcher(context)
             }
         }
-
-        launcher(context)
     }
 }
