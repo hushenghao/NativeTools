33f3576ced032f8fac4fc9b35d898677b9d2f459
胡晟昊 <863116814@qq.com>
2021-08-13 15:02:23 +0800

    关于页修改

---
+++ app/src/main/java/com/dede/nativetools/about/AboutViewModel.kt
@@ -0,0 +1,20 @@
+package com.dede.nativetools.about
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+/**
+ * 关于页，保存followCount
+ *
+ * @author hsh
+ * @since 2021/8/13 9:49 上午
+ */
+class AboutViewModel : ViewModel() {
+
+    val followCount = MutableLiveData(0)
+
+    fun setFollowCount(count: Int) {
+        followCount.value = count
+    }
+
+}
