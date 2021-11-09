ef88fd42676b1839278847d9a9b0bf1dba0f5575
胡晟昊 <863116814@qq.com>
2021-11-09 11:46:36 +0800

    添加了二维码保存功能

--- app/src/main/java/com/dede/nativetools/donate/DonateDialogFragment.kt
+++ app/src/main/java/com/dede/nativetools/donate/DonateDialogFragment.kt
@@ -1,21 +1,29 @@
 package com.dede.nativetools.donate
 
 import android.app.Dialog
+import android.content.Context
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.DrawableRes
 import androidx.core.view.ViewCompat
+import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.dede.nativetools.R
 import com.dede.nativetools.databinding.DialogFragmentDonateBinding
 import com.dede.nativetools.util.browse
+import com.dede.nativetools.util.saveToAlbum
 import com.dede.nativetools.util.toast
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import kotlin.math.roundToInt
 
 /**
@@ -39,8 +47,11 @@ class DonateDialogFragment : BottomSheetDialogFragment() {
             requireContext().browse(R.string.url_alipay_payment_code)
         }
         binding.ivWxpay.setOnClickListener {
-            toast("微信付款码请截图后扫描")
+            toast(R.string.toast_wx_payment_tip)
         }
+        binding.ivWxpay.setOnLongClickListener(createOnQrCodeLongClickListener(R.drawable.wx_payment_code))
+        binding.ivAlipay.setOnLongClickListener(createOnQrCodeLongClickListener(R.drawable.alipay_payment_code))
+
         binding.ivWxpay.run {
             post {
                 val padding = (1.5f / 51.5f * width).roundToInt()
@@ -60,5 +71,24 @@ class DonateDialogFragment : BottomSheetDialogFragment() {
         }
         return dialog
     }
+
+    private fun createOnQrCodeLongClickListener(@DrawableRes resId: Int): View.OnLongClickListener {
+        return View.OnLongClickListener {
+            lifecycleScope.launchWhenStarted {
+                val uri = saveToAlbum(requireContext(), resId)
+                if (uri != null) {
+                    toast(R.string.toast_saved)
+                }
+            }
+            return@OnLongClickListener true
+        }
+    }
+
+    private suspend fun saveToAlbum(context: Context, @DrawableRes resId: Int): Uri? =
+        withContext(Dispatchers.IO) {
+            val bitmap =
+                BitmapFactory.decodeResource(context.resources, resId) ?: return@withContext null
+            bitmap.saveToAlbum(requireContext(), "QrCode_${resId}.jpeg", "Net Monitor")
+        }
 
 }
