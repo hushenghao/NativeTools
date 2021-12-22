288661ff086dca69332eac95fc0cced1b05cbde9
胡晟昊 <863116814@qq.com>
2021-12-22 22:50:18 +0800

    feat: 捐赠页优化

--- app/src/main/java/com/dede/nativetools/donate/DonateDialogFragment.kt
+++ app/src/main/java/com/dede/nativetools/donate/DonateDialogFragment.kt
@@ -2,6 +2,8 @@ package com.dede.nativetools.donate
 
 import android.Manifest
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Build
@@ -13,9 +15,12 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.DrawableRes
 import androidx.core.graphics.drawable.toBitmap
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.dede.nativetools.R
 import com.dede.nativetools.databinding.DialogFragmentDonateBinding
+import com.dede.nativetools.databinding.ItemPaymentLayoutBinding
+import com.dede.nativetools.ui.SpaceItemDecoration
 import com.dede.nativetools.util.*
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.coroutines.Dispatchers
@@ -41,14 +46,61 @@ class DonateDialogFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.ivAlipay.setOnClickListener {
-            requireContext().browse(R.string.url_alipay_payment_code)
+        binding.recyclerView.adapter = Adapter(data)
+        binding.recyclerView.addItemDecoration(SpaceItemDecoration(12.dp))
+    }
+
+    private class Payment(
+        @DrawableRes
+        val resId: Int,
+        val click: View.OnClickListener,
+        val longClick: View.OnLongClickListener? = null
+    )
+
+    private val data = arrayOf(
+        Payment(
+            R.drawable.img_logo_wxpay, {
+                toast(R.string.toast_wx_payment_tip)
+            }, createOnLongClickSaveQrCodeListener(R.drawable.img_wx_payment_code)
+        ),
+        Payment(
+            R.drawable.img_logo_alipay, {
+                it.context.browse(R.string.url_alipay_payment_code)
+            }),
+        Payment(
+            R.drawable.img_logo_paypal, {
+                it.context.browse(R.string.url_paypal_payment_code)
+            })
+    )
+
+    private class Adapter(val data: Array<Payment>) : RecyclerView.Adapter<Holder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+            val itemView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_payment_layout, parent, false)
+            return Holder(itemView)
         }
-        binding.ivWxpay.setOnClickListener {
-            toast(R.string.toast_wx_payment_tip)
+
+        override fun onBindViewHolder(holder: Holder, position: Int) {
+            holder.bindViewData(data[position])
         }
-        binding.ivWxpay.setOnLongClickListener(createOnLongClickSaveQrCodeListener(R.drawable.layer_wx_payment_code))
-        binding.ivAlipay.setOnLongClickListener(createOnLongClickSaveQrCodeListener(R.drawable.layer_alipay_payment_code))
+
+        override fun getItemCount(): Int {
+            return data.size
+        }
+    }
+
+    private class Holder(view: View) : RecyclerView.ViewHolder(view) {
+        private val binding = ItemPaymentLayoutBinding.bind(itemView)
+
+        fun bindViewData(payment: Payment) {
+            binding.root.setOnClickListener(payment.click)
+            binding.root.setOnLongClickListener(payment.longClick)
+            binding.ivLogo.setImageResource(payment.resId)
+            if (isNightMode()) {
+                binding.ivLogo.imageTintList = ColorStateList.valueOf(Color.WHITE)
+            }
+        }
     }
 
     private fun createOnLongClickSaveQrCodeListener(@DrawableRes resId: Int): View.OnLongClickListener {
