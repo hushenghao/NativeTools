f174eb5c66ec5122904fca0f35b5acacc4fc7218
shhu <863116814@qq.com>
2022-01-04 13:46:23 +0800

    feat: SliderPreference功能完善
    添加默认值解析
    支持按键触发
    支持状态保存与恢复
    丰富了API

--- app/src/main/java/com/dede/nativetools/ui/SliderPreference.kt
+++ app/src/main/java/com/dede/nativetools/ui/SliderPreference.kt
@@ -1,9 +1,15 @@
 package com.dede.nativetools.ui
 
 import android.content.Context
+import android.content.res.TypedArray
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.view.HapticFeedbackConstants
+import android.view.KeyEvent
+import android.view.View
 import android.widget.TextView
+import androidx.annotation.Keep
 import androidx.preference.Preference
 import androidx.preference.PreferenceViewHolder
 import com.dede.nativetools.R
@@ -21,13 +27,14 @@ class SliderPreference @JvmOverloads constructor(
     defStyleAttr: Int = androidx.preference.R.attr.preferenceStyle,
     defStyleRes: Int = 0
 ) : Preference(context, attrs, defStyleAttr, defStyleRes), Slider.OnChangeListener,
-    Slider.OnSliderTouchListener {
+    Slider.OnSliderTouchListener, View.OnKeyListener {
 
-    private val stepSize: Float
+    private var stepSize: Float
     private var value: Float = 0f
-    private val valueFrom: Float
-    private val valueTo: Float
+    private var valueFrom: Float
+    private var valueTo: Float
 
+    private var slider: Slider? = null
     private var sliderValue: TextView? = null
 
     var sliderLabelFormatter: LabelFormatter? = null
@@ -46,11 +53,14 @@ class SliderPreference @JvmOverloads constructor(
             defStyleAttr,
             defStyleRes
         )
-        stepSize = typedArray.getFloat(R.styleable.SliderPreference_android_stepSize, 0.01f)
-        value = typedArray.getFloat(R.styleable.SliderPreference_android_value, value)
         valueFrom = typedArray.getFloat(R.styleable.SliderPreference_android_valueFrom, 0f)
         valueTo = typedArray.getFloat(R.styleable.SliderPreference_android_valueTo, 1f)
+        stepSize = typedArray.getFloat(R.styleable.SliderPreference_android_stepSize, 0.01f)
         typedArray.recycle()
+    }
+
+    override fun onGetDefaultValue(a: TypedArray?, index: Int): Any? {
+        return a?.getFloat(index, 0f)
     }
 
     override fun onSetInitialValue(defaultValue: Any?) {
@@ -67,10 +77,53 @@ class SliderPreference @JvmOverloads constructor(
         setValueInternal(value, true)
     }
 
-    private fun setValueInternal(value: Float, notify: Boolean) {
+    fun setStepSize(stepSize: Float) {
+        var size = stepSize
+        if (size < 0.0f) {
+            size = 0.01f
+        }
+        if (size != this.stepSize) {
+            this.stepSize = size
+            notifyChanged()
+        }
+    }
+
+    fun setValueFrom(valueFrom: Float) {
+        var from = valueFrom
+        if (from < valueTo) {
+            from = valueTo
+        }
+        if (from != this.valueFrom) {
+            this.valueFrom = from
+            notifyChanged()
+        }
+    }
+
+    fun getValueFrom(): Float {
+        return this.valueFrom
+    }
+
+    fun setValueTo(valueTo: Float) {
+        var to = valueTo
+        if (to > valueFrom) {
+            to = valueFrom
+        }
+        if (to != this.valueTo) {
+            this.valueTo = to
+            notifyChanged()
+        }
+    }
+
+    fun getValueTo(): Float {
+        return this.valueTo
+    }
+
+    private fun setValueInternal(sliderValue: Float, notify: Boolean) {
+        val value = max(valueFrom, min(sliderValue, valueTo))
         if (value != this.value) {
-            this.value = max(valueFrom, min(value, valueTo))
+            this.value = value
             persistFloat(this.value)
+            updateLabelValue(this.value)
             if (notify) {
                 notifyChanged()
             }
@@ -79,26 +132,40 @@ class SliderPreference @JvmOverloads constructor(
 
     override fun onBindViewHolder(holder: PreferenceViewHolder) {
         super.onBindViewHolder(holder)
-        val slider = holder.findViewById(R.id.slider) as? Slider
-        if (slider != null) {
-            slider.valueFrom = valueFrom
-            slider.valueTo = valueTo
-            slider.stepSize = stepSize
-            slider.value = value
-            slider.setLabelFormatter(sliderLabelFormatter)
-            slider.tag = key
+        holder.itemView.setOnKeyListener(this)
+        val slider = holder.findViewById(R.id.slider) as Slider
+        slider.valueFrom = valueFrom
+        slider.valueTo = valueTo
+        slider.stepSize = stepSize
+        slider.value = value
+        slider.setLabelFormatter(sliderLabelFormatter)
+        slider.tag = key
 
-            slider.removeOnChangeListener(this)
-            slider.removeOnSliderTouchListener(this)
-            slider.addOnChangeListener(this)
-            slider.addOnSliderTouchListener(this)
-        }
+        slider.removeOnChangeListener(this)
+        slider.removeOnSliderTouchListener(this)
+        slider.addOnChangeListener(this)
+        slider.addOnSliderTouchListener(this)
+        slider.isEnabled = isEnabled
+        this.slider = slider
 
         val sliderValue = holder.findViewById(R.id.slider_value) as? TextView
         if (sliderValue != null) {
             this.sliderValue = sliderValue
             updateLabelValue(value)
         }
+    }
+
+    override fun onKey(v: View?, keyCode: Int, event: KeyEvent): Boolean {
+        if (event.action != KeyEvent.ACTION_DOWN) {
+            return false
+        }
+
+        // We don't want to propagate the click keys down to the Slider view since it will
+        // create the ripple effect for the thumb.
+        if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER || keyCode == KeyEvent.KEYCODE_ENTER) {
+            return false
+        }
+        return slider?.onKeyDown(keyCode, event) ?: false
     }
 
     override fun onValueChange(slider: Slider, value: Float, fromUser: Boolean) {
@@ -128,5 +195,68 @@ class SliderPreference @JvmOverloads constructor(
         }
     }
 
+    override fun onSaveInstanceState(): Parcelable {
+        val superState = super.onSaveInstanceState()
+        if (isPersistent) {
+            return superState
+        }
+        val savedState = SavedState(superState)
+        savedState.stepSize = stepSize
+        savedState.value = value
+        savedState.valueFrom = valueFrom
+        savedState.valueTo = valueTo
+        return savedState
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        if (state !is SavedState) {
+            super.onRestoreInstanceState(state)
+            return
+        }
+        stepSize = state.stepSize
+        value = state.value
+        valueFrom = state.valueFrom
+        valueTo = state.valueTo
+    }
+
+    private class SavedState : BaseSavedState {
+
+        companion object {
+            @Keep
+            @JvmField
+            val CREATOR: Parcelable.Creator<SavedState?> =
+                object : Parcelable.Creator<SavedState?> {
+                    override fun createFromParcel(`in`: Parcel): SavedState {
+                        return SavedState(`in`)
+                    }
+
+                    override fun newArray(size: Int): Array<SavedState?> {
+                        return arrayOfNulls(size)
+                    }
+                }
+        }
+
+        var stepSize: Float = 0.01f
+        var value: Float = 0f
+        var valueFrom: Float = 0f
+        var valueTo: Float = 1f
+
+        constructor(source: Parcel) : super(source) {
+            stepSize = source.readFloat()
+            value = source.readFloat()
+            valueFrom = source.readFloat()
+            valueTo = source.readFloat()
+        }
+
+        constructor(superState: Parcelable) : super(superState)
+
+        override fun writeToParcel(dest: Parcel, flags: Int) {
+            super.writeToParcel(dest, flags)
+            dest.writeFloat(stepSize)
+            dest.writeFloat(value)
+            dest.writeFloat(valueFrom)
+            dest.writeFloat(valueTo)
+        }
+    }
 
 }
