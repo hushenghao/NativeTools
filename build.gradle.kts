c0fc454fc172121e79665e29b9fdaf8bf2e152b7
shhu <863116814@qq.com>
2022-01-10 16:23:38 +0800

    feat: tinify task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import com.tinify.Tinify
+import java.io.FileNotFoundException
+
 tasks.wrapper {
     distributionType = Wrapper.DistributionType.ALL
 }
@@ -8,10 +11,33 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath(deps.bundles.classpath)
+        classpath(deps.android.gradle)
+        classpath(deps.kotlin.gradle)
+        classpath(deps.diffplug.spotless)
+        classpath(deps.tinify)
     }
 }
 
 task<Delete>("clean") {
     delete(rootProject.buildDir)
+}
+
+task<Task>("tinify") {
+    description = "Tinify Compressing images"
+    val images = arrayOf(
+        "app/src/main/ic_launcher_day-playstore.png",
+        "app/src/main/ic_launcher_night-playstore.png",
+    )
+    doFirst {
+        Tinify.setKey("1Ny9z3LtggWW7TlnVj2jNkqRRHT9LFsK")// Free plan
+        Tinify.setAppIdentifier("Gradle task")
+        var file: File
+        for (image in images) {
+            file = File(project.projectDir, image)
+            if (!file.exists()) {
+                throw FileNotFoundException(file.path)
+            }
+            Tinify.fromFile(file.path).toFile(file.path)
+        }
+    }
 }
