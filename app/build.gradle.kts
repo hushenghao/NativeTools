1bc3b237f2b6cc3475b813f39d4dbccee1011f5a
胡晟昊 <863116814@qq.com>
2022-03-26 14:06:35 +0800

    chore: 回退compileSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ plugins {
 }
 
 android {
-//    compileSdk = 31
-    compileSdkPreview = "Tiramisu"
+    compileSdk = 31
+//    compileSdkPreview = "Tiramisu"
     buildToolsVersion = "32.0.0"
     defaultConfig {
         applicationId = "com.dede.nativetools"
